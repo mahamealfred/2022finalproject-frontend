621e7ad4f364db72e5b3e3f435096e874d5b21ab
blaise82 <izabayoblaise82@gmail.com>
2022-03-13 18:44:40 +0200

    BLaise-Last-Push

--- src/components/pages/assessment/Question.jsx
+++ src/components/pages/assessment/Question.jsx
@@ -7,7 +7,8 @@ import Header from "../../header/Header";
 import Footer from "../../Footer/Footer";
 import { useDispatch, useSelector } from "react-redux";
 import { getExamsAndQuestionByLevel } from "../../../redux/actions/getExamsAndQuestionByIdAction";
-import Questions from '../../Questions/Questions'
+import Questions from "../../Questions/Questions";
+import { Button } from "@mui/material";
 
 export default function Question({ ...rest }) {
   const getExamAndQuestionState = useSelector(
@@ -16,33 +17,84 @@ export default function Question({ ...rest }) {
   const dispatch = useDispatch();
   const [questions, setQuestions] = useState();
   const [score, setScore] = useState(0);
+  const [percentage, setPercentage] = useState(0);
+  const [remainingTime, setRemainingTime] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [fullName, setFullName] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [answers, setAnswers] = useState([]);
   const [examId, setExamId] = useState("");
-  const [exams, setExams] = useState([]);
 
-  const [options, setOptions] = useState();
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [numberQuestions, setNumberQuestion] = useState(1);
+  const studentData = JSON.parse(localStorage.getItem("student-data"));
+  // get fullname
+  console.log(studentData);
+  const fullname = studentData ? studentData.fullName : "Dear, Student";
 
-  // useEffect(() => {
-  //   setQuestions();
-  //   setOptions(
-  //     questions &&
-  //       handleShuffle([
-  //         questions[currentQuestion]?.correct_answer,
-  //         ...questions[currentQuestion]?.incorrect_answer,
-  //       ])
-  //   );
-  // }, [questions, currentQuestion]);
+  const clock = (hr, mm, ss) => {
+    var interval = setInterval(function () {
+      if (hr == 0 && mm == 0 && ss == 0) clearInterval(interval);
+      ss--;
+      if (ss == 0) {
+        ss = 59;
+        mm--;
+        if (mm == 0) {
+          mm = 59;
+          hr--;
+        }
+      }
+
+      if (hr.toString().length < 2) hr = "0" + hr;
+      if (mm.toString().length < 2) mm = "0" + mm;
+      if (ss.toString().length < 2) ss = "0" + ss;
+      setRemainingTime(hr + " : " + mm + " : " + ss);
+    }, 1000);
+  };
+
   useEffect(() => {
     async function fetchData() {
       await dispatch(getExamsAndQuestionByLevel(examId));
     }
+
+    clock(1, 1, 10);
     fetchData();
   }, []);
-  console.log(getExamAndQuestionState.exams);
 
   const handleShuffle = (options) => {
     return options.sort(() => Math.random() - 0.5);
+  };
+
+  const handleScore = (value, correct_answer, index) => {
+    // incase the index already exists in the score array update the value
+    if (answers[index]) {
+      answers[index] = { value, correct_answer, index };
+    }
+    // else add the index and value to the score array
+    else {
+      answers[index] = { value, correct_answer, index };
+    }
+  };
+
+  const handleSubmit = async () => {
+    // set loading to true
+    setIsLoading(true);
+    // loop through answers and calculate the final score
+    let final = 0;
+    await answers.forEach((answer) => {
+      console.log(answer, answer.value === answer.correct_answer);
+      if (answer.value === answer.correct_answer) {
+        console.log(final++);
+        setScore(final);
+      }
+    });
+    // set a percentage of the score
+    const percentage =
+      (final / getExamAndQuestionState.exams[0].questions.length) * 100;
+    console.log(getExamAndQuestionState.exams[0].questions.length);
+    setPercentage(Math.round(percentage));
+
+    // set loading to false
+    setIsSubmitted(true);
+    setIsLoading(false);
   };
 
   return (
@@ -53,7 +105,7 @@ export default function Question({ ...rest }) {
         "Loading"
       ) : getExamAndQuestionState.exams.length > 0 ? (
         <div className="assessmentQuestion">
-          <span className="subtitle">Welcome, Mahame Alfred</span>
+          <span className="subtitle">Welcome, {fullname}</span>
           {/* {
         questions?
           <> */}
@@ -69,27 +121,60 @@ export default function Question({ ...rest }) {
               </div>
               <div className="rightAssessmentInfo">
                 <div className="time">
-                  <span> Time left: 1h 30min</span>
+                  <span> Remaining Time: {remainingTime}</span>
                 </div>
-                <span> Question: {numberQuestions}/10 questions</span>
+                <span>
+                  {" "}
+                  Question: {answers.length}/
+                  {getExamAndQuestionState.exams[0].questions.length || 0}{" "}
+                  questions
+                </span>
               </div>
             </div>
           </>
-          <h1>"Questions"</h1>
-          {getExamAndQuestionState.exams[0].questions.map((question) => {
-            return <Questions question={question}/>
-          })}
-
-          {/* </>
-        :
-        <>
-        </>
-           <CircularProgress style={{margin:100}}
-           size={100}
-          thickness={1}
-           />
-        
-      } */}
+          {isSubmitted && !isLoading ? (
+            <div className="assessmentResult">
+              <div className="result">
+                <span>You have completed this test.</span>
+                <br />
+                <span>
+                  You have scored {score} out of{" "}
+                  {getExamAndQuestionState.exams[0].questions.length || 0}
+                </span>
+                <br />
+                <span>Your score is {percentage}%</span>
+              </div>
+            </div>
+          ) : (
+            <>
+              <h1>"Questions"</h1>
+              {getExamAndQuestionState.exams[0].questions.map(
+                (question, index) => {
+                  return (
+                    <Questions
+                      question={question}
+                      index={index}
+                      setScore={handleScore}
+                      score={score}
+                    />
+                  );
+                }
+              )}
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                disabled={
+                  answers.length <
+                  getExamAndQuestionState.exams[0].questions.length
+                }
+                style={{ width: 185 }}
+                onClick={() => handleSubmit()}
+              >
+                End And Submit
+              </Button>
+            </>
+          )}
         </div>
       ) : (
         "No Question found"
